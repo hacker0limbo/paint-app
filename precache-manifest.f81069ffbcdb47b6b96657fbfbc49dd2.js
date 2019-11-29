338209d6f1307bb849cf78a7f4f804b1ee533685
Stephen <stephen.yin@outlook.com>
2019-11-29 19:36:24 +0800

    Updates

---
+++ precache-manifest.f81069ffbcdb47b6b96657fbfbc49dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f3749e91cbfaf154034580f733b9df",
+    "url": "/paint-app/index.html"
+  },
+  {
+    "revision": "817e26e294e05d81aa6e",
+    "url": "/paint-app/static/css/main.7a7d36e0.chunk.css"
+  },
+  {
+    "revision": "8c5810439631f7b60138",
+    "url": "/paint-app/static/js/2.97fa161c.chunk.js"
+  },
+  {
+    "revision": "817e26e294e05d81aa6e",
+    "url": "/paint-app/static/js/main.54163c32.chunk.js"
+  },
+  {
+    "revision": "8dabd5d23f539ffeb402",
+    "url": "/paint-app/static/js/runtime-main.d64062cc.js"
+  }
+]);
